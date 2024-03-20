b287f5d16f453852870e329703b6f1c7bd04b44e
Vũ Quang Nhật <126136518+qnhat2004@users.noreply.github.com>
2024-03-20 22:42:33 +0700

    Add files via upload
    Add sql file

---
+++ QuanLyCuaHangInternet/Internet.sql
@@ -0,0 +1,17 @@
+﻿create database QL_Internet;
+use QL_Internet;
+
+Create table BangKe (
+	STT INT IDENTITY primary key,
+	SoMay nvarchar(30),
+	GioVao int,
+	GioRa int,
+	SoGio int,
+	DonGia int, 
+	ThanhTien int
+);
+
+
+INSERT INTO BangKe (SoMay, GioVao, GioRa, SoGio, DonGia, ThanhTien) VALUES ('Máy 1', 5, 12, 7, 5000, 35000);
+
+Select STT, SoMay as 'Số máy', GioVao as 'Giờ vào', GioRa as 'Giờ ra', SoGio as 'Số giờ sử dụng', DonGia as 'Đơn giá/Giờ', ThanhTien as 'Thành tiền' From BangKe
